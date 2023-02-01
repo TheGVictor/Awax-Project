0ff0081d54f7161394862cf52c276fd2e6cfcf1f
Gabriel Sancinetti <gabrielsancinetti@gmail.com>
2023-02-01 09:53:51 -0300

    arquivo JS adicionado

---
+++ awax.js
@@ -0,0 +1,15 @@
+let abaNav = document.querySelector('nav')
+let img = document.querySelector('.menu-opener')
+
+function openMenu() {
+
+    abaNav.style.display = "block"
+
+    if(abaNav.style.display = "block") {
+            abaNav.style.display = "none"
+        }
+    
+    
+}
+
+img.addEventListener('click', openMenu)
